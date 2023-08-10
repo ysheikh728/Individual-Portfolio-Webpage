b04b7e446f7c81cc0d460895d982fc102aab5561
ysheikh728 <yahya.sheikh1998@gmail.com>
2023-08-10 03:24:31 -0400

    added mailscript

--- app/components/_documents.js
+++ app/components/_documents.js
@@ -7,7 +7,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           {/* Other meta tags, stylesheets, etc. */}
-          <script id="mcjs">/* Mailchimp script here */</script>
+          <script id="mcjs"><script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/7cb4f38d508b4cf09d7c3b353/619bca3ceead2f39ecbed42f5.js");</script></script>
         </Head>
         <body>
           <Main />
