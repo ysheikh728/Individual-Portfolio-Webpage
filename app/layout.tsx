abcec5e9e0a3b804a6346c628ee85b887222b1de
ysheikh728 <134982259+ysheikh728@users.noreply.github.com>
2023-08-10 18:29:17 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'; // Import the Script component
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Individual Porfolio',
+  title: 'Individual Portfolio',
   description: 'A look into the world of Yahya',
 }
 
@@ -16,6 +17,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Other head elements... */}
+
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-M9RX98XYNX`}
+        />
+        <Script strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-M9RX98XYNX');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
